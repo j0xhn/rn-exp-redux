23c97d0ac4b0d8a25edec26b46d168baede0a69d
John D. Storey <johndangerstorey@gmail.com>
2016-06-25 19:31:48 -0600

    gets a scrollable tab views working

--- index.ios.js
+++ index.ios.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ScrollableTabView from 'react-native-scrollable-tab-view'
 import { AppRegistry } from 'react-native'
 
 import configureStore from './app/store/configureStore'
@@ -9,7 +10,11 @@ import { Provider } from 'react-redux'
 
 const App = () => (
   <Provider store={store}>
-    <NavigationRootContainer />
+    <ScrollableTabView>
+      <NavigationRootContainer tabLabel="1"/>
+      <NavigationRootContainer tabLabel="2"/>
+      <NavigationRootContainer tabLabel="3"/>
+    </ScrollableTabView>
   </Provider>
 )
 AppRegistry.registerComponent('RNExperimentalRedux', () => App)
